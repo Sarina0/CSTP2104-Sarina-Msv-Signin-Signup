e338c9e77f5b19303cf6ebf701d3aa47ce30fc85
sarina <sarina.mosuavi18@gmail.com>
2022-05-13 11:43:25 -0700

    Add project files.

---
+++ CSTP2104/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSTP2104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
