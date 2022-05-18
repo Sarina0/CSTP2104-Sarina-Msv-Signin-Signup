1194c96dd9cb79b173219e2efbdfb80544905213
sarina <sarina.mosuavi18@gmail.com>
2022-05-18 00:39:31 -0700

    assignment2

---
+++ CSTP2104/Assignment2/CalculateSum.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSTP2104.Assignment2
+{
+    class CalculateSum
+    {
+        public void RunTasks()
+        {
+            Task.Run(() => Console.WriteLine("Sum of Number task!"));
+            new Thread(() => Console.WriteLine("Start")).Start();
+            int sum = 0;
+            Task task1 = Task.Run(() =>
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    sum = sum + j;
+                    PrintSum(sum);
+                   
+
+                }
+            });
+            
+        }
+       
+
+        public void PrintSum(int s)
+        {
+            Console.Write("\nThe Sum is => {0}\n", s);
+            
+        }
+
+
+        public static void WriteProgressToFileAsync(int txt)
+        {
+            string path = @"C:\Users\sarin\source\repos\CSTP2104\CSTP2104\Assignment2\SumReport.txt";
+            try
+            {
+                File.WriteAllText(path, txt.ToString());
+                Console.WriteLine("String written to file successfully.");
+                try
+                {
+                    // Open the text file using a stream reader.
+                    using (var sr = new StreamReader(path))
+                    {
+                        // Read the stream as a string, and write the string to the console.
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+    }
+}
+
--- CSTP2104/Program.cs
+++ CSTP2104/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 using CSTP2104.Assignment1;
-
+using CSTP2104.Assignment2;
 
 namespace CSTP2104
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //Initialing the Report
+        public static async Task Main(string[] args)
+        {    //Assignment1
+
+            /*//Initialing the Report
             ProgressReporter del1 = UtilReporter.InitialReport;
             UtilReporter.ReportProgress(del1);
 
@@ -18,7 +20,17 @@ namespace CSTP2104
 
             //Writing to the file
             ProgressReporter del3 = UtilReporter.WriteProgressToFileAsync;
-            UtilReporter.ReportProgress(del3);
+            UtilReporter.ReportProgress(del3);*/
+
+            //Assignment2
+
+            CalSum();
+
+        }
+        public static void CalSum()
+        {
+            var ut = new CalculateSum();
+            ut.RunTasks();
         }
     }
 }
